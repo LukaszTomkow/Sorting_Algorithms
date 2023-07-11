131d74cdb4a9adf6e1257bf9899c96e8fb897f78
Łukasz Tomków <lukasz.tomkow97@gmail.com>
2023-07-11 12:15:36 +0200

    merge sort update

--- Merge_sort.py
+++ Merge_sort.py
@@ -1,6 +1,19 @@
 import json
 
-with open("dataset_3.json", 'r') as f:
+with open("dataset_0.json", 'r') as f:
     data = json.load(f)
-    
+
 print(data)
+
+def merge_sort(data):
+    if len(data) > 1:
+        #cut in half
+        print(data)
+        merge_sort(data[0,len(data)/2])
+    
+    else:
+        #merge remaning data with other list
+        print(data)
+
+
+merge_sort(data)
